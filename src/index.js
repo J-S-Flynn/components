9a388259c81d3d2ac0921b52619381eacf70224c
Jason Flynn <j.s.flynn@Jasons-MacBook-Pro.local>
2022-05-07 18:11:20 +0100

    fobar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom'; 
+import faker from 'faker'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return (
+        <div className="ui container comment">
+            <div className="comment">
+                <a href="" classNme="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a className="author" href="">
+                        Sma
+                    </a>
+                    <div classNme="metadata">
+                        <span classNme="date"> Today at 6:00pm </span>
+                    </div>
+                    <div classNme="text"> Nice Blog Post  </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
+ReactDom.render(<App />, document.querySelector('#root'));
